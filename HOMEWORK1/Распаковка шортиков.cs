82eb7dcc9c67706777d584d23eec80a9b38869c0
claudiusis <velikiy_2004@bk.ru>
2022-11-23 20:48:56 +0300

    First homework

---
+++ HOMEWORK1/Распаковка шортиков.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WASP_HOMEWORK
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            short[] numbers = new short[4];
+            short k = 32767;
+            Console.Write("Введите \"запакованные шортики\"");
+            long chislo = long.Parse(Console.ReadLine());
+            for (int i= 0; i < 4; i++)
+            {
+                numbers[i] = (short)(chislo&k);
+                chislo >>= 16;
+            }
+            for (int i=0; i < 4; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ HOMEWORK1/Треугольник Паскаля.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WASP_HOMEWORK
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное число n - высоту треугольника Паскаля: ");
+            ulong n = Convert.ToUInt16(Console.ReadLine());
+            ulong k;
+            for (ulong i=0; i < n; i++)
+            {
+                k = 1;
+                Console.Write(k);
+                for (ulong j = 1; j <= i; j++)
+                {
+                    k *= i - j + 1;           //Что если заменить на:
+                    k /= j;                  //k = (k * (i - j + 1)) / j;
+                    Console.Write(" ");
+                    Console.Write(k);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
